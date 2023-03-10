14f3ce02c65ba79ae11e39787b397274f19f7f2e
TV Hop <hoptran67@gmail.com>
2023-03-10 09:08:30 +0700

    day code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Alo alo
+         Alo alo s sdas
         </p>
         <a
           className="App-link"
