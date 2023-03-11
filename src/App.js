2e2dc73f130ffb765d1763ac015033023994057e
TV Hop <hoptran67@gmail.com>
2023-03-11 12:29:40 +0700

    day code 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Alo alo s sdas
+         asd
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactss
         </a>
       </header>
     </div>
