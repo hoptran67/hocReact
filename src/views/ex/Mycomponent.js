85c7d9439e348583155ec22d3049753917d72a2c
TV Hop <hoptran67@gmail.com>
2023-03-20 00:16:34 +0700

    day code20 03

--- src/views/ex/Mycomponent.js
+++ src/views/ex/Mycomponent.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 class MyComponent extends React.Component {
 
+state = {
+    firtsName:'',
+    lastName:''
+}
+
+
+
+    handleChangefirtsName = (event) =>{
+        this.setState({
+            firtsName: event.target.value
+        })
+
+    }
+    handleChangelastName = (event) =>{
+        this.setState({
+            lastName : event.target.value
+        })
+    }
+
     render() {
+        console.log('sdfasd:',this.state.firtsName);
+        console.log('sdfasd:',this.state.lastName);
+    
         return (
-            <div>hello my component Hoi Dan IT</div>
+            <>
+            <form >
+            <label for="fname">First name:</label>
+            <input
+             type="text"value={this.state.firtsName}
+                onChange= {(event)=> this.handleChangefirtsName(event)}
+             
+             />
+             <br/>
+             
+            <label for="lname">Last name:</label>
+            <input 
+            type="text"  value={this.state.lastName}
+            onChange={(event)=> this.handleChangelastName(event)}
+            
+            /><br/><br/>
+            <input type="submit" value="Submit"/>
+            </form> 
+            </>
         )
 
     }
