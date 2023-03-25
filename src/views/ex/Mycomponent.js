878dbe1dd87d78a65d10db833d099208ca901177
TV Hop <hoptran67@gmail.com>
2023-03-25 21:03:44 +0700

    day code25 03

--- src/views/ex/Mycomponent.js
+++ src/views/ex/Mycomponent.js
@@ -20,15 +20,18 @@ state = {
             lastName : event.target.value
         })
     }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log('>>>>Check data input:',this.state);
+    }
 
     render() {
-        console.log('sdfasd:',this.state.firtsName);
-        console.log('sdfasd:',this.state.lastName);
-    
+        console.log('>>>>Call render:',this.state);
+        
         return (
             <>
             <form >
-            <label for="fname">First name:</label>
+            <label htmlFor="fname">First name:</label>
             <input
              type="text"value={this.state.firtsName}
                 onChange= {(event)=> this.handleChangefirtsName(event)}
@@ -36,13 +39,16 @@ state = {
              />
              <br/>
              
-            <label for="lname">Last name:</label>
+            <label htmlFor="lname">Last name:</label>
             <input 
             type="text"  value={this.state.lastName}
             onChange={(event)=> this.handleChangelastName(event)}
             
             /><br/><br/>
-            <input type="submit" value="Submit"/>
+            <input type="button" value="submit"
+            onClick={(event)=> this.handleSubmit(event)}
+            
+            />
             </form> 
             </>
         )
